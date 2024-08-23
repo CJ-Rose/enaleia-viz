4ad5cefa384d60822851ac66d520ab1c25f97cc7
CJ Rose <cjrose59@gmail.com>
2024-08-22 17:15:14 -0700

    replace map with choropleth component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,121 @@
 "use client"
-import { ResponsiveGeoMap } from '@nivo/geo'
+import { ResponsiveChoropleth } from '@nivo/geo'
 import { Feature, Geometry, GeoJsonProperties } from 'geojson';
 import features from './features.geo.json'
+import data from './data.json'
 
 interface MediterraneanGeoMapProps {
   features: Feature[];
+  data: { id: string; value: number }[];
 }
 
-const MediterraneanGeoMap = ({ features }: MediterraneanGeoMapProps) => (
-  <ResponsiveGeoMap
+const MediterraneanGeoMap = ({ features, data }: MediterraneanGeoMapProps) => (
+  <ResponsiveChoropleth
+      data={data}
       features={features}
       margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+      colors="nivo"
+      domain={[ 0, 1000000 ]}
+      unknownColor="#666666"
+      label="properties.name"
+      valueFormat=".2s"
+      projectionScale={400}
       projectionTranslation={[ 0.5, 0.5 ]}
       projectionRotation={[ 0, 0, 0 ]}
-      fillColor="#eeeeee"
-      borderWidth={0.5}
-      borderColor="#333333"
       enableGraticule={false}
-      graticuleLineColor="#666666"
+      graticuleLineColor="#dddddd"
+      borderWidth={0.5}
+      borderColor="#152538"
+      defs={[
+          {
+              id: 'dots',
+              type: 'patternDots',
+              background: 'inherit',
+              color: '#38bcb2',
+              size: 4,
+              padding: 1,
+              stagger: true
+          },
+          {
+              id: 'lines',
+              type: 'patternLines',
+              background: 'inherit',
+              color: '#eed312',
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10
+          },
+          {
+              id: 'gradient',
+              type: 'linearGradient',
+              colors: [
+                  {
+                      offset: 0,
+                      color: '#000'
+                  },
+                  {
+                      offset: 100,
+                      color: 'inherit'
+                  }
+              ]
+          }
+      ]}
+      fill={[
+          {
+              match: {
+                  id: 'CAN'
+              },
+              id: 'dots'
+          },
+          {
+              match: {
+                  id: 'CHN'
+              },
+              id: 'lines'
+          },
+          {
+              match: {
+                  id: 'ATA'
+              },
+              id: 'gradient'
+          }
+      ]}
+      legends={[
+          {
+              anchor: 'bottom-left',
+              direction: 'column',
+              justify: true,
+              translateX: 20,
+              translateY: -100,
+              itemsSpacing: 0,
+              itemWidth: 94,
+              itemHeight: 18,
+              itemDirection: 'left-to-right',
+              itemTextColor: '#444444',
+              itemOpacity: 0.85,
+              symbolSize: 18,
+              effects: [
+                  {
+                      on: 'hover',
+                      style: {
+                          itemTextColor: '#000000',
+                          itemOpacity: 1
+                      }
+                  }
+              ]
+          }
+      ]}
   />
 )
+
 
 export default function Home() {
   return (
     <main className='h-screen'>
-      <MediterraneanGeoMap features={features.features as Feature<Geometry, GeoJsonProperties>[]}/>
+      <MediterraneanGeoMap 
+        features={features.features as Feature<Geometry, GeoJsonProperties>[]}
+        data={data}
+      />
     </main>
   );
 }
